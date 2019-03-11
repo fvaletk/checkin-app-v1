6bd43aff1500c398135624f4d86f90643c183a4c
Filiberto Valetk <fili.valetk@gmail.com>
2019-03-11 10:59:07 -0500

    [002] Add routes

---
+++ src/Dashboard/Dashboard.tsx
@@ -0,0 +1,11 @@
+/** Dependencies */
+import React, { Component } from 'react';
+
+/** Methods */
+export class Dashboard extends Component {
+  render() {
+    return(
+      <h1>Hello From Dashboard</h1>
+    )
+  }
+}
